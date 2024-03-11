3daf17d9f93e2ec95a668c8c1ed0031dc4f5faaf
bhumitgoyal <lifestyleprojects.rg@gmail.com>
2024-03-11 14:45:27 +0530

    sdcsdsef

---
+++ app/src/main/java/com/example/study_buddy/AddItemActivity.kt
@@ -0,0 +1,21 @@
+package com.example.study_buddy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.study_buddy.databinding.ActivityAddItemBinding
+
+class AddItemActivity : AppCompatActivity() {
+
+    private val binding : ActivityAddItemBinding by lazy{
+        ActivityAddItemBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
--- app/src/main/java/com/example/study_buddy/MainActivity.kt
+++ app/src/main/java/com/example/study_buddy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.study_buddy
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,13 +8,22 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.study_buddy.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+        binding.addRequest.setOnClickListener {
+            val intent = Intent(this,AddItemActivity::class.java)
+            startActivity(intent)
+        }
         var NavController = findNavController(R.id.fragmentContainerView)
         var bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomnav.setupWithNavController(NavController)
